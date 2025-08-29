84c3120f170eb8c65fb592bdc5232e9128353c8d
k4hr <alecsandr-7a@yandex.ru>
2025-08-29 14:27:23 +0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import { PT_Serif } from "next/font/google";
+
+const ptSerif = PT_Serif({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "700"],
+});
+
+export const metadata = {
+  title: "Juristum",
+  description: "Юридическое приложение",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ru">
+      <body className={ptSerif.className}>{children}</body>
+    </html>
+  );
+}
