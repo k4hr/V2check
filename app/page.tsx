7c6fef8bf9b71eaa85e2a36ea2b2949265ded24a
k4hr <alecsandr-7a@yandex.ru>
2025-08-30 20:36:19 +0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ import TopBar from "./components/TopBar";
 import BigButton from "./components/BigButton";
 import { isPro } from "./lib/subscription";
 
+import { applyTelegramThemeVars, getTg } from "./lib/tma";
+
 export default function Home() {
   const [pro, setPro] = useState(false);
 
   useEffect(() => {
     try {
       const w: any = window;
-      w?.Telegram?.WebApp?.ready?.();
+      w?.Telegram?.WebApp?.ready?.(); applyTelegramThemeVars();
       w?.Telegram?.WebApp?.expand?.();
     } catch {}
     setPro(isPro());
