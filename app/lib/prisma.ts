52abee62792c5b3a13c103449a6674c705278097
k4hr <alecsandr-7a@yandex.ru>
2025-08-31 11:12:58 +0300

    Add files via upload

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,9 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({ log: ['error'] });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const g = global as unknown as { prisma?: PrismaClient };
+export const prisma = g.prisma || new PrismaClient({ log: ['error'] });
+if (process.env.NODE_ENV !== 'production') g.prisma = prisma;
