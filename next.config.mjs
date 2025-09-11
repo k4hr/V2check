770b0057962dcb005c23503015be37e79358f59b
k4hr <alecsandr-7a@yandex.ru>
2025-09-11 03:28:13 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Собираем серверный бандл, БЕЗ static export.
+  // Собираем серверный бандл для `next start`, без static export:
   output: 'standalone',
   experimental: { typedRoutes: true },
 };
+
 export default nextConfig;
