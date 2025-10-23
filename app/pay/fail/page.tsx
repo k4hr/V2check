07d439537f65d74db1c330eedfbd2ad753007302
k4hr <alecsandr-7a@yandex.ru>
2025-10-24 00:06:31 +0300

    Create page.tsx

---
+++ app/pay/fail/page.tsx
@@ -0,0 +1,8 @@
+export default function FailPage() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Платёж не выполнен ❌</h1>
+      <p>Похоже, произошла ошибка или вы отменили оплату.</p>
+    </main>
+  );
+}
