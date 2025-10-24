7a20cd594f4072441c09e2acc9d01f2969f4467e
k4hr <alecsandr-7a@yandex.ru>
2025-10-25 01:53:12 +0300

    Delete app/pay directory

--- app/pay/fail/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function FailPage() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Платёж не выполнен ❌</h1>
-      <p>Похоже, произошла ошибка или вы отменили оплату.</p>
-    </main>
-  );
-}
--- app/pay/success/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SuccessPage() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Платёж прошёл успешно ✅</h1>
-      <p>Спасибо за оплату! Можете вернуться в приложение.</p>
-    </main>
-  );
-}
